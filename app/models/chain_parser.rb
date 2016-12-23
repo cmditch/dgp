5def59052d6e599c884ec0d5f29a5348e08ff8ba
cmditch <cmditch@gmail.com>
2016-12-23 01:05:12 -0700

    [FEAT] scheduler updates

--- app/models/chain_parser.rb
+++ app/models/chain_parser.rb
@@ -2,35 +2,39 @@ module ChainParser
   require 'blockcypher'
 
     def self.parse_btc(wallet)
-      api = BlockCypher::Api.new(currency: wallet.currency)
-      txs = api.address_details(wallet.address)["txrefs"]
+      begin
+        api = BlockCypher::Api.new(currency: wallet.currency)
+        txs = api.address_details(wallet.address)["txrefs"]
 
-      # [REFAC] delete recorded transactions before calling api below
+        # [REFAC] delete recorded transactions before calling api below
 
-      txs.each do |tx_load|
-        sleep 0.2
-        tx = api.blockchain_transaction(tx_load["tx_hash"])
-        txid                = tx_load["tx_hash"]
-        sender_addresses    = tx["inputs"].map {|sender| sender["addresses"]}.flatten
-        receiving_addresses = tx["outputs"].map {|receiver| receiver["addresses"]}.flatten
-        amount              = tx["total"]
-        fee                 = tx["fees"]
-        time                = tx["received"]
-        block               = tx["block_height"]
+        txs.each do |tx_load|
+          sleep 0.2
+          tx = api.blockchain_transaction(tx_load["tx_hash"])
+          txid                = tx_load["tx_hash"]
+          sender_addresses    = tx["inputs"].map {|sender| sender["addresses"]}.flatten
+          receiving_addresses = tx["outputs"].map {|receiver| receiver["addresses"]}.flatten
+          amount              = tx["total"]
+          fee                 = tx["fees"]
+          time                = tx["received"]
+          block               = tx["block_height"]
 
-          transaction_params  = {
-          txid: txid,
-          sender: sender_addresses,
-          recipient: receiving_addresses,
-          amount: amount,
-          time: time.to_datetime,
-          block_id: block,
-          fee: fee,
-          currency: "btc"
-        }
-        unless Transaction.list.include?(transaction_params[:txid])
-          Transaction.create(transaction_params)
+            transaction_params  = {
+            txid: txid,
+            sender: sender_addresses,
+            recipient: receiving_addresses,
+            amount: amount,
+            time: time.to_datetime,
+            block_id: block,
+            fee: fee,
+            currency: "btc"
+          }
+          unless Transaction.list.include?(transaction_params[:txid])
+            Transaction.create(transaction_params)
+          end
         end
+      rescue => e
+        puts "!!! Parsing failed for WalletID: #{wallet.id} !!!"
       end
     end
 
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,8 +1,9 @@
 desc "This task adds recent BTC txs to the list"
 task :update_btc_txs => :environment do
   puts "Updating feed..."
-  Wallet.each do |wallet|
+  Wallet.all.each do |wallet|
     ChainParser.parse_btc wallet
+    puts "Finished parsing transactions for WalletID: #{wallet.id}"
   end
   puts "done."
 end
