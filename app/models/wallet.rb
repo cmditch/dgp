0c6aef8db01b013db0542b0621a7c95f140315fa
cmditch <cmditch@gmail.com>
2016-12-25 01:02:25 -0700

    [FEAT] Adding interfaces to wallet

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,3 +1,52 @@
 class Wallet < ActiveRecord::Base
+  require 'blockcypher'
+
   belongs_to :transactor, polymorphic: true
+
+  def api
+    BlockCypher::Api.new(currency: currency)
+  end
+
+  def update_balance
+    balance = api.address_balance(address)["balance"]
+    case currency
+    when "eth"
+      self.balance /= 10**16
+    when "btc"
+      self.balance /= 10**8
+    end
+    save
+  end
+
+  def txs
+    address = self.address
+    txs = Transaction.all
+    txs.map { |tx| tx if [tx.sender, tx.recipient].flatten.include?(address) }.compact
+  end
+
+  def txs_in
+    address = self.address
+    txs = Transaction.all
+    txs.map { |tx| tx if tx.recipient.include?(address) }.compact
+  end
+
+  def txs_out
+    address = self.address
+    txs = Transaction.all
+    txs.map { |tx| tx if tx.sender.include?(address) }.compact
+  end
+
+  def tx_ids_out
+    address = self.address
+    txs = Transaction.all
+    txs.map { |tx| tx.txid if tx.sender.include?(address) }.compact
+  end
+
+  def tx_ids_in
+    address = self.address
+    txs = Transaction.all
+    txs.map { |tx| tx.txid if tx.recipient.include?(address) }.compact
+  end
+
 end
+
